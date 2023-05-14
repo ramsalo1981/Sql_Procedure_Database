3010d60c744aa8ec55150a8bcb9fa26022fc042a
ramsalo1981 <rami.salloum@hotmail.com>
2023-05-14 18:36:38 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace My_Database_Procedure
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCount = new System.Windows.Forms.Button();
+            this.dgvUsers = new System.Windows.Forms.DataGridView();
+            this.dgvEmail = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmail)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.White;
+            this.lblName.Location = new System.Drawing.Point(13, 13);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(92, 29);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(18, 46);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(299, 36);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(18, 120);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(299, 36);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.ForeColor = System.Drawing.Color.White;
+            this.lblEmail.Location = new System.Drawing.Point(13, 90);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(88, 29);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "Email:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(18, 197);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(299, 36);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPass.ForeColor = System.Drawing.Color.White;
+            this.lblPass.Location = new System.Drawing.Point(13, 164);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(137, 29);
+            this.lblPass.TabIndex = 4;
+            this.lblPass.Text = "Password:";
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDisplay.Location = new System.Drawing.Point(95, 239);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(140, 41);
+            this.btnDisplay.TabIndex = 6;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(95, 286);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(140, 41);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(95, 333);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(140, 41);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnCount
+            // 
+            this.btnCount.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCount.Location = new System.Drawing.Point(95, 380);
+            this.btnCount.Name = "btnCount";
+            this.btnCount.Size = new System.Drawing.Size(140, 41);
+            this.btnCount.TabIndex = 9;
+            this.btnCount.Text = "Count";
+            this.btnCount.UseVisualStyleBackColor = true;
+            // 
+            // dgvUsers
+            // 
+            this.dgvUsers.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvUsers.BackgroundColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvUsers.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsers.Location = new System.Drawing.Point(359, 13);
+            this.dgvUsers.Name = "dgvUsers";
+            this.dgvUsers.RowHeadersWidth = 62;
+            this.dgvUsers.RowTemplate.Height = 28;
+            this.dgvUsers.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dgvUsers.Size = new System.Drawing.Size(353, 447);
+            this.dgvUsers.TabIndex = 10;
+            // 
+            // dgvEmail
+            // 
+            this.dgvEmail.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvEmail.BackgroundColor = System.Drawing.Color.White;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvEmail.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dgvEmail.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmail.Location = new System.Drawing.Point(718, 12);
+            this.dgvEmail.Name = "dgvEmail";
+            this.dgvEmail.RowHeadersWidth = 62;
+            this.dgvEmail.RowTemplate.Height = 28;
+            this.dgvEmail.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dgvEmail.Size = new System.Drawing.Size(353, 447);
+            this.dgvEmail.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Navy;
+            this.ClientSize = new System.Drawing.Size(1076, 472);
+            this.Controls.Add(this.dgvEmail);
+            this.Controls.Add(this.dgvUsers);
+            this.Controls.Add(this.btnCount);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmail)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCount;
+        private System.Windows.Forms.DataGridView dgvUsers;
+        private System.Windows.Forms.DataGridView dgvEmail;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace My_Database_Procedure
+{
+    public partial class Form1 : Form
+    {
+        static string strConn = ConfigurationManager.ConnectionStrings["myConn"].ConnectionString;
+        SqlConnection conn = new SqlConnection(strConn);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            //***** Display data by using DataSet ******
+
+            string sql = "SELECT TB_Users.Name, TB_Users.Email FROM TB_Users; " +
+                         "SELECT TB_Users.Name, TB_Users.Password FROM TB_Users;";
+
+            SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            ds.Tables[0].TableName = "NameEmail";
+            ds.Tables[1].TableName = "NamePassword";
+            dgvUsers.DataSource = ds.Tables["NameEmail"];
+            dgvEmail.DataSource = ds.Tables["NamePassword"];
+
+
+            //***** Display Data by using sqlDataAdaptar *****
+
+            //string sql = "SELECT * FROM TB_Users;";
+            //SqlDataAdapter ad = new SqlDataAdapter(sql,conn);
+            //DataTable dt = new DataTable();
+            //ad.Fill(dt);
+            //dgvUsers.DataSource = dt;
+
+
+            //***** Display data by using datatable *****
+
+            //string sql = "SELECT * FROM TB_Users;";
+            //SqlCommand sqlComm = new SqlCommand(sql, conn);
+
+            //DataTable dt = new DataTable();
+            //dt.Columns.Add("Name");
+            //dt.Columns.Add("Email");
+            //dt.Columns.Add("Password");
+
+            //conn.Open();
+
+            //using (SqlDataReader dr = sqlComm.ExecuteReader())
+            //{
+            //    while (dr.Read())
+            //    {
+            //        DataRow row = dt.NewRow();
+            //        row["Name"] = dr[0];
+            //        row["Email"] = dr[1];
+            //        row["Password"] = dr[2];
+
+            //        dt.Rows.Add(row);
+
+            //    }
+
+            //    dgvUsers.DataSource = dt;
+            //}
+
+
+            //********Display data on datagridview*****
+
+            //string sql = "Select * FROM TB_Users;";
+            //SqlCommand sqlComm = new SqlCommand(sql, conn);
+            //conn.Open();
+
+            //using (SqlDataReader dr = sqlComm.ExecuteReader())
+            //{
+            //    dgvUsers.Columns.Add("Name", "Name");
+            //    dgvUsers.Columns.Add("Email", "Email");
+            //    dgvUsers.Columns.Add("Password", "Password");
+
+            //    while (dr.Read())
+            //    {
+            //        dgvUsers.Rows.Add(dr[0], dr[1], dr[2]);
+            //    }
+            //}
+
+
+
+            // *******Display data in Messegebox******
+
+            //SqlCommand sqlComm = new SqlCommand("sp_selectData", conn);
+            //sqlComm.CommandType = CommandType.StoredProcedure;
+
+
+            //try
+            //{
+            //    conn.Open();
+            //    var dr = sqlComm.ExecuteReader();
+            //    while (dr.Read())
+            //    {
+            //        MessageBox.Show($"{dr[0].ToString()} {dr[1].ToString()} {dr[2].ToString()}");
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    MessageBox.Show($"Error!! \n {ex}");
+            //}
+            //finally { conn.Close(); }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            //use using
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                SqlCommand sqlComm = new SqlCommand("sp_InsertData", conn);
+                sqlComm.CommandType = CommandType.StoredProcedure;
+                sqlComm.Parameters.AddWithValue("@name", txtName.Text);
+                sqlComm.Parameters.AddWithValue("@email", txtEmail.Text);
+                sqlComm.Parameters.AddWithValue("@password", txtPassword.Text);
+                conn.Open();
+                sqlComm.ExecuteNonQuery();
+            }
+
+            //use try and catch*********
+
+            //SqlCommand sqlComm = new SqlCommand("sp_InsertData", conn);
+            //sqlComm.CommandType = CommandType.StoredProcedure;
+            //sqlComm.Parameters.AddWithValue("@name", txtName.Text);
+            //sqlComm.Parameters.AddWithValue("@email", txtEmail.Text);
+            //sqlComm.Parameters.AddWithValue("@password", txtPassword.Text);
+
+            //try
+            //{
+            //    conn.Open();
+            //    sqlComm.ExecuteNonQuery();
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    MessageBox.Show($"Error!! \n {ex}");
+
+            //}
+            //finally { conn.Close(); }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string sql = $"DELETE FROM TB_Users WHERE name = @pName; ";
+            SqlCommand sqlComm = new SqlCommand(sql,conn);
+            sqlComm.Parameters.AddWithValue("@pName",txtName.Text);
+            conn.Open();
+
+            sqlComm.ExecuteNonQuery();
+
+            conn.Close();
+
+            //string sql = $"DELETE FROM TB_Users WHERE name = '{txtName.Text}'; ";
+            //SqlCommand sqlComm = new SqlCommand(sql,conn);
+            //conn.Open();
+            //sqlComm.ExecuteNonQuery();
+            //conn.Close();
+        }
+    }
+}
